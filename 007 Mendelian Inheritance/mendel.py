bbdf6ceb820ff929ad84f59397b0eb5880c9ec5f
ckmah <clarence.iyan@gmail.com>
2015-02-12 13:10:05 -0800

    added comments to 007

--- 007 Mendelian Inheritance/mendel.py
+++ 007 Mendelian Inheritance/mendel.py
@@ -1,3 +1,7 @@
+# !/usr/bin/env python
+# # author: Clarence Mah
+# Given: Three positive integers k, m, and n, representing a population containing k+m+n organisms: k individuals are homozygous dominant for a factor, m are heterozygous, and n are homozygous recessive.
+# Return: The probability that two randomly selected mating organisms will produce an individual possessing a dominant allele (and thus displaying the dominant phenotype). Assume that any two organisms can mate.
 from sys import argv
 
 script, filename = argv
